26466616bad9a5484509694c3ca9cae97f0f6fb3
Paulo Henrique Dias Lopes <106744519+PauloHDL@users.noreply.github.com>
2025-10-23 05:38:32 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
-function Home (){
-    return <h1>Teste de ammend</h1>
+function Home() {
+  return <h1>Teste de ammend</h1>;
 }
 
 export default Home;
 
+function Vision() {
+  return <h2>lala</h2>;
+}
 
-function Vision () {
-    return <h2>lala</h2>
+function Teste() {
+  console.log("Teste do prettier  ");
 }
